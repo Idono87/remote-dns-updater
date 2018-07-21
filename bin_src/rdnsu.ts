9bc65f4d0466a4aacfddc2287903a798fd44c31c
Unknown <sorin.sandru@idono.net>
2018-07-21 10:24:00 +0200

    Fixed Start Issues
    Fixed start issues caused by not properly finding the the app.js file.

--- bin_src/rdnsu.ts
+++ bin_src/rdnsu.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-import yargs = require('yargs');
-import winston = require('winston');
-import InstanceLocker = require('instance-locker');
-import { spawn, ChildProcess } from 'child_process';
+import yargs = require("yargs");
+import winston = require("winston");
+import InstanceLocker = require("instance-locker");
+import { spawn, ChildProcess } from "child_process";
+import path = require("path");
 
 // TODO: Create a better launching script.
 /////////////////////////////////////////////////////////////
@@ -16,9 +17,9 @@ winston.debug("Initializing 'Winston'");
 winston.setLevels(winston.config.npm.levels);
 
 const cOptions: winston.ConsoleTransportOptions = {
-    level: 'silly',
-    colorize: true,
-    timestamp: true,
+  level: "silly",
+  colorize: true,
+  timestamp: true
 };
 
 winston.clear();
@@ -30,165 +31,166 @@ winston.handleExceptions();
 /////////////////////////////////////////////////////////////
 
 /* debug */
-winston.debug('Initializing Yargs Commands');
+winston.debug("Initializing Yargs Commands");
 /* debug-end */
 
 yargs.help(false);
 yargs
-    .alias('verbose', 'v')
-    .boolean('verbose')
-    .nargs('verbose', 0)
-    .default('verbose', false);
+  .alias("verbose", "v")
+  .boolean("verbose")
+  .nargs("verbose", 0)
+  .default("verbose", false);
 
-yargs.command('start', 'Start the application.', {}, start);
-yargs.command('restart', 'Restart the application.', {}, restart);
-yargs.command('stop', 'Stop the application.', {}, stop);
-yargs.command('$0', 'Default Start in verbose', {}, defaultStart);
+yargs.command("start", "Start the application.", {}, start);
+yargs.command("restart", "Restart the application.", {}, restart);
+yargs.command("stop", "Stop the application.", {}, stop);
+yargs.command("$0", "Default Start in verbose", {}, defaultStart);
 
 /////////////////////////////////////////////////////////////
 //// Commands
 /////////////////////////////////////////////////////////////
-process.env.LOCK_NAME = 'Remote DNS Updater';
+process.env.LOCK_NAME = "Remote DNS Updater";
 
 const instanceLock: InstanceLocker.LockerAsync = InstanceLocker(
-    process.env.LOCK_NAME,
-    false,
-    true,
+  process.env.LOCK_NAME,
+  false,
+  true
 );
 
 async function start(): Promise<void> {
-    //Exit if an instance is already running.
-    let pid: number = await instanceLock.GetOwnerPID();
-    try {
-        process.kill(pid, 0);
-        winston.info('Remote DNS Updater is already running.');
-        exit(0);
-    } catch (err) {
-        if (err.code !== 'ESRCH') {
-            winston.error(err.message);
-            exit(0);
-            return;
-        }
+  //Exit if an instance is already running.
+  let pid: number = await instanceLock.GetOwnerPID();
+  try {
+    if (pid !== -1) {
+      process.kill(pid, 0);
+      winston.info("Remote DNS Updater is already running.");
+      exit(0);
     }
+  } catch (err) {
+    if (err.code !== "ESRCH") {
+      winston.error(err.message);
+      exit(0);
+      return;
+    }
+  }
 
-    const env = {
-        LOCK_NAME: process.env.LOCK_NAME,
-    };
+  const env = {
+    LOCK_NAME: process.env.LOCK_NAME
+  };
 
-    const options = {
-        cwd: process.cwd(),
-        stdio: 'ignore',
-        env: env,
-        detached: true,
-    };
+  const options = {
+    cwd: process.cwd(),
+    stdio: "ignore",
+    env: env,
+    detached: true
+  };
 
-    let subProcess: ChildProcess = spawn(
-        'node',
-        ['./lib/app.js', ...process.argv.slice(2)],
-        options,
-    );
+  let subProcess: ChildProcess = spawn(
+    "node",
+    ["../lib/app.js", ...process.argv.slice(2)],
+    options
+  );
 
-    subProcess.on('error', (err) => {
-        winston.error(err.message);
+  subProcess.on("error", err => {
+    winston.error(err.message);
+    exit(0);
+  });
+
+  subProcess.unref();
+
+  let hasStartedCount: number = 0;
+  setInterval(async () => {
+    pid = await instanceLock.GetOwnerPID();
+    try {
+      if (pid !== -1) {
+        process.kill(pid, 0);
+        winston.info("Application has started successfully.");
         exit(0);
-    });
-
-    subProcess.unref();
-
-    let hasStartedCount: number = 0;
-    setInterval(async () => {
-        pid = await instanceLock.GetOwnerPID();
-        try {
-            process.kill(pid, 0);
-            winston.info('Application has started successfully.');
-            exit(0);
-            return;
-        } catch (err) {}
-        hasStartedCount++;
-        if (hasStartedCount >= 10) {
-            winston.info('Could not start application.');
-        }
-    }, 500);
+        return;
+      }
+    } catch (err) {}
+    hasStartedCount++;
+    if (hasStartedCount >= 10) {
+      winston.info("Could not start application.");
+    }
+  }, 500);
 }
 
 async function restart(): Promise<void> {
-    if (await stop()) {
-        await start();
-    }
+  if (await stop()) {
+    await start();
+  }
 }
 
 async function stop(): Promise<boolean> {
-    let exitInterval: NodeJS.Timer;
+  let exitInterval: NodeJS.Timer;
 
-    try {
-        let pid: number = await instanceLock.GetOwnerPID();
-        if (pid == -1) {
-            winston.info('The application is not running.');
-            return false;
-        }
-
-        process.kill(pid, 'SIGINT');
-
-        //Test if process has exited.
-        await new Promise((resolve, reject) => {
-            let retries: number = 0;
-            const maxRetries: number = 10;
-            exitInterval = setInterval(() => {
-                try {
-                    retries++;
-                    process.kill(pid, 0);
-                } catch (err) {
-                    clearInterval(exitInterval);
-                    reject(err);
-                    return;
-                }
-
-                if (retries >= maxRetries) {
-                    clearInterval(exitInterval);
-                    resolve(false);
-                    return;
-                }
-            }, 1000);
-        });
-
-        winston.info('Failed to stop the application.');
-        return false;
-    } catch (err) {
-        if (err.code !== 'ESRCH') {
-            winston.error('Failed to stop the application.');
-            return false;
-        }
+  try {
+    let pid: number = await instanceLock.GetOwnerPID();
+    if (pid === -1) {
+      winston.info("The application is not running.");
+      return false;
     }
 
-    winston.info('Application has stopped');
-    return true;
+    process.kill(pid, "SIGINT");
+
+    //Test if process has exited.
+    await new Promise((resolve, reject) => {
+      let retries: number = 0;
+      const maxRetries: number = 10;
+      exitInterval = setInterval(() => {
+        try {
+          retries++;
+          process.kill(pid, 0);
+        } catch (err) {
+          clearInterval(exitInterval);
+          reject(err);
+          return;
+        }
+
+        if (retries >= maxRetries) {
+          clearInterval(exitInterval);
+          resolve(false);
+          return;
+        }
+      }, 1000);
+    });
+
+    winston.info("Failed to stop the application.");
+    return false;
+  } catch (err) {
+    if (err.code !== "ESRCH") {
+      winston.error("Failed to stop the application.");
+      return false;
+    }
+  }
+
+  winston.info("Application has stopped");
+  return true;
 }
 
 async function defaultStart(): Promise<void> {
-    const env = {
-        STARTED_FROM: process.env.STARTED_FROM,
-        LOCK_NAME: process.env.LOCK_NAME,
-    };
+  const env = {
+    STARTED_FROM: process.env.STARTED_FROM,
+    LOCK_NAME: process.env.LOCK_NAME
+  };
 
-    const options = {
-        cwd: process.cwd(),
-        stdio: 'inherit',
-        env: env,
-        detached: false,
-    };
+  const options = {
+    cwd: path.join(__dirname, ".."),
+    stdio: "inherit",
+    env: env,
+    detached: false
+  };
 
-    let subProcess: ChildProcess = spawn(
-        'node',
-        ['./lib/app.js', ...process.argv.slice(2)],
-        options,
-    );
-
-    winston.error('Parent Process: ' + process.pid.toString());
-    winston.error('SubProcess: ' + subProcess.pid.toString());
+  let subProcess: ChildProcess = spawn(
+    "node",
+    ["./lib/app.js", ...process.argv.slice(2)],
+    options
+  );
 }
 
 function exit(code: number): void {
-    process.kill(code);
+  process.kill(code);
 }
 
 let args: any = yargs.parse();
--- src/app.ts
+++ src/app.ts
@@ -1,65 +1,65 @@
-import winston = require('winston');
-import Configuration from './init';
-import RemoteUpdater from './remoteUpdater';
-import InstanceLocker = require('instance-locker');
-import ConfigurationObject from './configurationObject';
+import winston = require("winston");
+import Configuration from "./init";
+import RemoteUpdater from "./remoteUpdater";
+import InstanceLocker = require("instance-locker");
+import ConfigurationObject from "./configurationObject";
 
 const instanceLock: InstanceLocker.LockerAsync = InstanceLocker(
-    <string>process.env.LOCK_NAME,
-    false,
-    true,
+  <string>process.env.LOCK_NAME,
+  false,
+  true
 );
 let updater: RemoteUpdater;
 let interval: NodeJS.Timer;
 
-(async function run(): Promise<void> {
-    if (!instanceLock.Lock()) {
-        winston.info('Application is already running');
-        exit(0);
-    }
+async function run(): Promise<void> {
+  if (!instanceLock.Lock()) {
+    winston.info("Application is already running");
+    exit(0);
+  }
 
-    let nIntervalMin =
-        <number>(<ConfigurationObject>Configuration).update_interval *
-        1000 *
-        60;
+  let nIntervalMin =
+    <number>(<ConfigurationObject>Configuration).update_interval * 1000 * 60;
 
-    updater = new RemoteUpdater(<ConfigurationObject>Configuration);
-    interval = setInterval(update, nIntervalMin);
-    await update();
+  updater = new RemoteUpdater(<ConfigurationObject>Configuration);
+  interval = setInterval(update, nIntervalMin);
+  await update();
 
-    return;
-})();
+  return;
+}
 
 async function update(): Promise<void> {
-    try {
-        await updater.update();
-    } catch (err) {
-        winston.error(
-            'The following error has ocurred while trying to update.',
-        );
-        winston.error(err);
-        kill(1);
-    }
+  try {
+    await updater.update();
+  } catch (err) {
+    winston.error("The following error has ocurred while trying to update.");
+    winston.error(err);
+    kill(1);
+  }
 }
 
 async function exit(code: number) {
-    clearInterval(interval);
-    await instanceLock.Unlock();
-    process.exitCode = code;
+  clearInterval(interval);
+  await instanceLock.Unlock();
+  process.exitCode = code;
 }
 
 function kill(code: number) {
-    process.exit(code);
+  process.exit(code);
 }
 
-process.on('SIGINT', () => {
-    exit(0);
+process.on("SIGINT", () => {
+  exit(0);
 });
-process.on('SIGHUP', () => {
-    exit(0);
+process.on("SIGHUP", () => {
+  exit(0);
 });
 
-process.on('uncaughtException', (err) => {
-    winston.error(err.message);
-    kill(0);
+process.on("uncaughtException", err => {
+  winston.error(err.message);
+  kill(0);
+});
+
+run().catch(err => {
+  throw err;
 });
